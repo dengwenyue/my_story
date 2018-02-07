0aa7d919f27b012b63d526511a50dce0b1fd91d6
dengwenyue <dengwenyue@b.360.cn>
2018-02-07 10:57:17 +0800

    修改某些信息

--- test_read.py
+++ test_read.py
@@ -7,9 +7,9 @@ import requests
 TOKEN_URL = "https://openapi.baidu.com/oauth/2.0/token?grant_type={0}&client_id={1}&client_secret={2}"
 TEXT2AUDIO_URL = 'https://tsn.baidu.com/text2audio?tex={0}&lan=zh&cuid={1}&ctp=1&tok={2}&spd={3}&pit={4}&vol={5}&per={6}'
 GRANT_TYPE = "client_credentials"
-CUID = "dengwenyue123"
-Client_id = "Xmb1lGy8THbzcozXt0mQhpWP"
-Secret_key = "8ae1fc7365113f12115cf94ebec0e770"
+CUID = "XXXXXXXXXXXXX"
+Client_id = "XXXXXXXXXXXX"
+Secret_key = "XXXXX"
 
 
 def get_token():
@@ -43,17 +43,6 @@ def text2audit(text, spd=5, pit=5, vol=6, per=4):
     return b"".join(result)
 
 
-# with open("/tmp/第2章.txt", 'r') as f:
-#     text = f.read()
-#
-# print text
-# print type(text)
-text = "亚麻跌" * 50
-audio = text2audit(text=text)
-
-
-with open("test.mp3", "wb") as f:
-    f.write(audio)
 
 
 
